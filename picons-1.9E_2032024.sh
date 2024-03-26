74ec8a4878dde0176dd77b6d729a01d7ee30bd04
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-26 14:09:48 +0200

    Add files via upload

---
+++ picons-1.9E_2032024.sh
@@ -0,0 +1,102 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/picons-1.9E_2032024.tar.gz "https://raw.githubusercontent.com/Ham-ahmed/picons/main/picons-1.9E_2032024.tar.gz"
+
+echo ""
+# Download and install
+cd /tmp
+set -e
+tar -xzf picons-1.9E_2032024.tar.gz -C /
+set +e
+rm -f picons-1.9E_2032024.tar.gz
+
+echo ""
+sync
+echo "##############################################################"
+echo "#              PICONS INSTALLED SUCCESSFULLY                 #"
+echo "#                UPLOADED BY Hamdy Ahmed                     #"
+echo "##############################################################"
+
+echo "#              RESTART YOUR Recever                          #"
+echo "##############################################################"
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
