f474a699df486d329a10eaece953e4b91ccc3fe3
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-29 02:14:13 +0200

    Add files via upload

---
+++ picons_All-Sat_28-3-2024.sh
@@ -0,0 +1,102 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/picons_All-Sat_28-3-2024.tar.gz "https://gitlab.com/h-ahmed/Panel/-/raw/main/picons_All-Sat_28-3-2024.tar.gz"
+
+echo ""
+# Download and install
+cd /tmp
+set -e
+tar -xzf picons_All-Sat_28-3-2024.tar.gz -C /
+set +e
+rm -f picons_All-Sat_28-3-2024.tar.gz
+
+echo ""
+sync
+echo "##############################################################"
+echo "#              PICONS INSTALLED SUCCESSFULLY                 #"
+echo "#                UPLOADED BY Hamdy Ahmed                     #"
+echo "##############################################################"
+
+echo "#              RESTART YOUR Recever                          #"
+echo "##############################################################"
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
